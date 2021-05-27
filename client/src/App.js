93cc7b963ce14490fea51001a284e687b0241bc5
Mohetti <moritz.hettich@gmx.de>
2021-05-27 12:08:42 +0300

    Initializing Project

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div>Hello World</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
